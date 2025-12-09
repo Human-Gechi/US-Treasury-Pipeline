c2b980b4c33ff25b04930df696a4545ccbd3eed0
Human-Gechi <kimberlydaniel363@gmail.com>
2025-12-09 13:13:40 +0100

    code

--- Api/main.py
+++ Api/main.py
@@ -1,9 +1,11 @@
 import os
 import sys
-from fastapi import FastAPI, Query, Depends
+from fastapi import FastAPI, Query, Depends, HTTPException, Security
+from fastapi.security import APIKeyHeader
 from typing import Optional
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
+
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
@@ -11,13 +13,11 @@ from Data.models import *
 
 load_dotenv()
 
-
 app = FastAPI(
     title="US treasury data",
     version="1.0.0",
-    description="Application Interface Programming for Average rate of US securities"
+    description="Application Programming Interface for Average rate of US securities"
 )
-
 
 app.add_middleware(
     CORSMiddleware,
@@ -26,6 +26,15 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+API_KEY_NAME = "Records_API_Key"
+api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=True)
+API_KEY = os.getenv("API_KEY")
+
+async def validate_keys(api_key: str = Security(api_key_header)):
+    if api_key != API_KEY:
+        raise HTTPException(status_code=401, detail="Invalid API Key")
+    return api_key
 
 @app.on_event("startup")
 async def startup_event():
@@ -39,12 +48,12 @@ async def shutdown_event():
 async def root():
     return {"message": "Average Rate US Treasury API is running"}
 
-@app.get("/records/")
+@app.get("/records", dependencies=[Depends(validate_keys)])
 async def all_records(
     db_connection = Depends(get_conn),
     page: int = Query(1, ge=1),
     size: int = Query(50, ge=1, le=100),
-    all_records: bool = Query(False, alias="all")
+    all_records: bool = Query(False, alias="all"),
 ):
     skip_amount = (page - 1) * size
     if all_records:
@@ -55,7 +64,7 @@ async def all_records(
         results = {"Record": records, "page": page, "size": size, "offset": skip_amount}
     return results
 
-@app.get("/records/record_count")
+@app.get("/records/record_count", dependencies=[Depends(validate_keys)])
 async def total_records(
     db_connection = Depends(get_conn)
 ):
@@ -64,7 +73,7 @@ async def total_records(
 
     return result
 
-@app.get("/records/latest")
+@app.get("/records/latest", dependencies=[Depends(validate_keys)])
 async def latest_record(
     db_connection = Depends(get_conn)
 ):
@@ -73,7 +82,7 @@ async def latest_record(
 
     return result
 
-@app.get("/records/types")
+@app.get("/records/types", dependencies=[Depends(validate_keys)])
 async def get_security_types(
     db_connection = Depends(get_conn)
 ):
@@ -82,7 +91,7 @@ async def get_security_types(
 
     return result
 
-@app.get("/records/by-date")
+@app.get("/records/by-date" , dependencies=[Depends(validate_keys)])
 async def get_records_date(
     db_connection = Depends(get_conn),
     year: Optional[int] = Query(None, description="Filter date by year(e.g YYYY)"),
@@ -92,7 +101,7 @@ async def get_records_date(
     record = await fetch_by_date(conn=db_connection, year=year, month=month , day=day)
     return {"Record" : record}
 
-@app.get("/records/by-security-type/")
+@app.get("/records/by-security-type/" , dependencies=[Depends(validate_keys)])
 async def get_records_by_security_type(
     db_connection = Depends(get_conn),
     security_type: str = Query(..., description="Filter by security type description i.e security_type_desc")
--- start.sh
+++ start.sh
@@ -1,4 +1,1 @@
-#!/bin/bash
-echo "Starting API endpoint"
 
-python -m uvicorn Api.main:app --host 0.0.0.0 --port 8080
